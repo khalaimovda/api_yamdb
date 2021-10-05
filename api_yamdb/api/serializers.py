1839e35d8e92cce03a1bb6c6dfce8dfa63ff437c
Khalaimov Dmitrii <halaimov_15@mail.ru>
2021-10-06 01:57:15 +0300

    added jwt, corrected jwt, added UserViewSet

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,6 +1,23 @@
-from rest_framework import serializers
+from django.conf import settings
+from django.shortcuts import get_object_or_404
+from django.contrib.auth import authenticate, get_user_model
+from django.contrib.auth.models import update_last_login
+from django.utils.translation import gettext_lazy as _
+from rest_framework import exceptions, serializers
+from rest_framework_simplejwt.settings import api_settings
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.serializers import PasswordField
 
 from reviews.models import Categories, Genres, Titles, Reviews, Comments
+
+User = get_user_model()
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'first_name',
+                  'last_name', 'bio', 'role', )
 
 
 class CommentsSerializer(serializers.ModelSerializer):
@@ -42,3 +59,55 @@ class TitlesSerializer(serializers.ModelSerializer):
     class Meta:
         fields = '__all__'
         model = Titles
+
+
+class TokenObtainSerializer(serializers.Serializer):
+    username_field = User.USERNAME_FIELD
+    password_field = settings.PASSWORD_FIELD
+
+    default_error_messages = {
+        'no_active_account': _(
+            'No active account found with the given credentials'
+        )
+    }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields[self.username_field] = serializers.CharField()
+        self.fields[self.password_field] = PasswordField()
+
+    def validate(self, attrs):
+        authenticate_kwargs = {
+            self.username_field: attrs[self.username_field],
+            'password': attrs[self.password_field],
+        }
+        try:
+            authenticate_kwargs['request'] = self.context['request']
+        except KeyError:
+            pass
+
+        get_object_or_404(
+            klass=User,
+            username=attrs[self.username_field]
+        )
+
+        self.user = authenticate(**authenticate_kwargs)
+
+        if not api_settings.USER_AUTHENTICATION_RULE(self.user):
+            raise exceptions.AuthenticationFailed(
+                self.error_messages['no_active_account'],
+                'no_active_account',
+            )
+
+        refresh = self.get_token(self.user)
+        data = {'token': str(refresh.access_token)}
+
+        if api_settings.UPDATE_LAST_LOGIN:
+            update_last_login(None, self.user)
+
+        return data
+
+    @classmethod
+    def get_token(cls, user):
+        return RefreshToken.for_user(user)
