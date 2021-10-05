5cb5fce38a324195eeefadc2bc35415057031bbf
Роман <romangrbr@gmail.com>
2021-10-05 22:09:46 +0300

    add-serializers-pattern

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -4,20 +4,41 @@ from ..reviews.models import Categories, Genres, Titles, Reviews, Comments
 
 
 class CommentsSerializer(serializers.ModelSerializer):
-    pass
+    author = serializers.SlugRelatedField(
+        read_only=True, slug_field='username'
+    )
+
+    class Meta:
+        fields = ('id', 'author', 'pub_date', 'text', 'review')
+        model = Comments
 
 
 class ReviewsSerializer(serializers.ModelSerializer):
-    pass
+    author = serializers.SlugRelatedField(
+        read_only=True, slug_field='username'
+    )
+
+    class Meta:
+        fields = ('id', 'author', 'pub_date', 'score', 'text', 'title')
+        model = Reviews
 
 
 class CategoriesSerializer(serializers.ModelSerializer):
-    pass
+
+    class Meta:
+        fields = '__all__'
+        model = Categories
 
 
 class GenresSerializer(serializers.ModelSerializer):
-    pass
+
+    class Meta:
+        fields = '__all__'
+        model = Genres
 
 
 class TitlesSerializer(serializers.ModelSerializer):
-    pass
+
+    class Meta:
+        fields = '__all__'
+        model = Titles
