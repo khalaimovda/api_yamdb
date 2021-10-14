be6fd07ea948293584fbe1c6ef090bbe50d1d827
Khalaimov Dmitrii <halaimov_15@mail.ru>
2021-10-14 20:51:35 +0300

    added read_only_fields to UserMeSerializer

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -25,13 +25,11 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class UserMeSerializer(serializers.ModelSerializer):
-
-    role = serializers.CharField(read_only=True)
-
     class Meta:
         model = User
         fields = ('username', 'email', 'first_name',
                   'last_name', 'bio', 'role', )
+        read_only_fields = ('role', )
         extra_kwargs = {
             'username': {'required': False},
             'email': {'required': False},
